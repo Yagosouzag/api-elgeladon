0f01614442f792d7a9254e5ba2d8be1f1ad874e7
Yagosouzag <yagomatheuscecilia@gmail.com>
2022-04-18 18:51:16 -0300

    Initial Commit

--- index.js
+++ index.js
@@ -1,48 +1,16 @@
 const express = require('express');
 const cors = require('cors');
+const routes = require('./src/routes/routes');
+const connectToDatabase = require('./src/database/database');
 
 const port = 3000;
 const app = express();
 
+connectToDatabase();
+
 app.use(express.json());
 app.use(cors());
-
-const paletas = [
-  {
-    id: 1,
-    sabor: 'Açaí com Leite Condensado',
-    descricao:
-      'Quam vulputate dignissim suspendisse in est ante in nibh mauris.',
-    foto: 'assets/images/acai-com-leite-condensado.png',
-    preco: 10.0,
-  },
-  {
-    id: 2,
-    sabor: 'Banana com Nutella',
-    descricao:
-      'Quam vulputate dignissim suspendisse in est ante in nibh mauris.',
-    foto: 'assets/images/banana-com-nutella.png',
-    preco: 10.0,
-  },
-  {
-    id: 3,
-    sabor: 'Chocolate Belga',
-    descricao:
-      'Quam vulputate dignissim suspendisse in est ante in nibh mauris.',
-    foto: 'assets/images/chocolate-belga.png',
-    preco: 7.0,
-  },
-];
-
-app.get('/paletas/todas-paletas', (req, res) => {
-  res.send(paletas);
-});
-
-app.get('/paletas/paleta/:id', (req, res) => {
-  const parametroId = Number(req.params.id);
-  const escolhaPaleta = paletas.find((paleta) => paleta.id === parametroId);
-  res.send(escolhaPaleta);
-});
+app.use('/paletas', routes);
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
